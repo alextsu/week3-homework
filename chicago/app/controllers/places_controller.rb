c6a2dda934e904c6dd14147cde72f996997e8f1a
alextsu <alextsu@gmail.com>
2015-04-22 04:02:55 -0500

    show works

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -5,7 +5,13 @@ class PlacesController < ApplicationController
 	end
 
 	def show
+		@place = Place.find_by(:id => params["id"])
 
+		if @place != nil
+
+		else
+			redirect_to "/", notice: "Movie not found."
+		end
 	end
 
 end
