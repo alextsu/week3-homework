0593e1cfbb9c49bfaae1e2daa0c0df055d6763cf
alextsu <alextsu@gmail.com>
2015-04-22 23:28:19 -0500

    Fixed attribute error.

--- chicago/app/models/review.rb
+++ chicago/app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
-  attr_accessible :reviewed_place_id
-  attr_accessible :title
-  attr_accessible :rating
-  attr_accessible :description
+  # attr_accessible :reviewed_place_id
+  # attr_accessible :title
+  # attr_accessible :rating
+  # attr_accessible :description
 end
